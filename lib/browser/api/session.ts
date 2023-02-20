e91bd0142f40c32bc3212f17ee343962e36863a3
Jeremy Rose <jeremya@chromium.org>
2023-02-20 12:57:38 -0800

    feat: implement net.fetch (#36733)

--- lib/browser/api/session.ts
+++ lib/browser/api/session.ts
@@ -1,5 +1,5 @@
 import { fetchWithSession } from '@electron/internal/browser/api/net-fetch';
-const { fromPartition,fromPath, Session } = process._linkedBinding('electron_browser_session');
+const { fromPartition, fromPath, Session } = process._linkedBinding('electron_browser_session');
 
 Session.prototype.fetch = function (input: RequestInfo, init?: RequestInit) {
   return fetchWithSession(input, init, this);
