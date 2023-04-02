0be15e9e22ad26274e1081ef7c2621ef0537fa2b
George Kottackal <kottackal.george@gmail.com>
2023-04-03 00:08:02 +0100

    Fixup! remove old test case and modify after()

--- spec/api-session-spec.ts
+++ spec/api-session-spec.ts
@@ -31,30 +31,6 @@ describe('session module', () => {
     });
   });
 
-  describe('session.fromPartition(partition, options)', () => {
-    afterEach(closeAllWindows);
-    it('assert that a set quota value is returned when a quota value is specified for a session', async () => {
-      const quotasize = 256000;
-      const localsession = session.fromPartition('persist:test', { cache: false, quota: quotasize });
-      const w = new BrowserWindow({
-        show: false,
-        webPreferences: {
-          session: localsession
-        }
-      });
-
-      const readQuotaSize: any = () => {
-        return w.webContents.executeJavaScript(`
-          navigator.storage.estimate().then(estimate => estimate.quota).catch(err => err.message);
-        `);
-      };
-
-      await w.loadURL('https://www.google.com');
-      const size = await readQuotaSize();
-      expect(size).to.equal(quotasize);
-    });
-  });
-
   describe('session.fromPath(path)', () => {
     it('returns storage path of a session which was created with an absolute path', () => {
       const tmppath = require('electron').app.getPath('temp');
@@ -64,9 +40,14 @@ describe('session module', () => {
   });
 
   describe('session.fromPath(path) with a quota.', () => {
+    const pathloc = 'testsession';
+    after(() => {
+      const tmppath = require('electron').app.getPath('temp') + path.sep + pathloc;
+      if (fs.existsSync(tmppath)) { fs.rmSync(tmppath, { recursive: true, force: true }); }
+    });
     it('Assert that a set quota value is returned when a quota value is specified for a session', async () => {
       const quotasize = 256000;
-      const tmppath = require('electron').app.getPath('temp') + path.sep + 'testsession';
+      const tmppath = require('electron').app.getPath('temp') + path.sep + pathloc;
       if (fs.existsSync(tmppath)) { fs.rmSync(tmppath, { recursive: true, force: true }); }
       fs.mkdirSync(tmppath);
       const localsession = session.fromPath(tmppath, { quota: quotasize });
