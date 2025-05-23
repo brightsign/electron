ed5b74b062c28bd2cb24bed2a81883dc2ee4d612
Tariq Bashir <tbashir@brightsign.biz>
2025-05-23 11:31:47 +0100

    feat: Add support for window rotation OS-17652 (#65)
    * OS-17652: Add support for window rotation
    
    * Mention windowTransform webpreference is for Linux
    
    * Add window rotation test case
    
    ---------
    
    Signed-off-by: Tariq Bashir <120014322+t-bashir-bs@users.noreply.github.com>
    
    test: Improve html window rotation tests (#67)
    
    Improve html window rotation tests
    
    fix: Add missing implementation for rotating popups in html widgets  (#72)
    
    * Fix rotated popup windows
    
    * Add extra test for popup window rotation
    
    Cherry picked from:
    0947088444a5d230fbab341057eb6d2937b41d25: feat: Add support for window rotation OS-17652 (#65)
    5d0988413e28f4f65b0eee894372e01342dfaa22: test: Improve html window rotation tests (#67)
    96a6d5d0f31f05de9a940fc3f085e6e5abc67538: fix: Add missing implementation for rotating popups in html widgets  (#72)

--- shell/browser/api/electron_api_base_window.cc
+++ shell/browser/api/electron_api_base_window.cc
@@ -1091,6 +1091,25 @@ bool BaseWindow::IsSnapped() const {
 }
 #endif
 
+#if BUILDFLAG(IS_LINUX)
+void BaseWindow::SetWindowTransform(const std::string& transform) {
+  blink::mojom::WindowTransformType transform_type =
+      blink::mojom::WindowTransformType::kWindowTransformTypeNone;
+
+  if (transform == "rot180") {
+    transform_type =
+        blink::mojom::WindowTransformType::kWindowTransformTypeRotate180;
+  } else if (transform == "rot270") {
+    transform_type =
+        blink::mojom::WindowTransformType::kWindowTransformTypeRotate270;
+  } else if (transform == "rot90") {
+    transform_type =
+        blink::mojom::WindowTransformType::kWindowTransformTypeRotate90;
+  }
+  window_->SetWindowTransform(transform_type);
+}
+#endif
+
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX)
 void BaseWindow::SetTitleBarOverlay(const gin_helper::Dictionary& options,
                                     gin_helper::Arguments* args) {
@@ -1344,6 +1363,9 @@ void BaseWindow::BuildPrototype(v8::Isolate* isolate,
 #endif
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX)
       .SetMethod("setTitleBarOverlay", &BaseWindow::SetTitleBarOverlay)
+#endif
+#if BUILDFLAG(IS_LINUX)
+      .SetMethod("setWindowTransform", &BaseWindow::SetWindowTransform)
 #endif
       .SetProperty("id", &BaseWindow::GetID);
 }
