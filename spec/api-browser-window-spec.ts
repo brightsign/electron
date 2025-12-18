544b5f5fbf0c82c8048d0835ea20875e24e8c0b1
Tariq Bashir <tbashir@brightsign.biz>
2025-12-18 14:58:34 +0000

    fix: Fix test specs that rely on chrome-error

--- spec/api-browser-window-spec.ts
+++ spec/api-browser-window-spec.ts
@@ -3716,11 +3716,21 @@ describe('BrowserWindow module', () => {
           }
         });
 
+        const server = http.createServer((req, res) => {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end('<html><body>Blank Page</body></html>');
+        });
+        const serverUrl = (await listen(server)).url;
+
         const preloadPath = path.join(mainFixtures, 'api', 'new-window-preload.js');
         w.webContents.setWindowOpenHandler(() => ({ action: 'allow', overrideBrowserWindowOptions: { webPreferences: { preload: preloadPath } } }));
-        w.loadFile(path.join(fixtures, 'api', 'new-window.html'));
+        await w.loadFile(path.join(fixtures, 'api', 'new-window.html'), {
+          query: { url: serverUrl }
+        });
         const [, { argv }] = await once(ipcMain, 'answer');
         expect(argv).to.include('--enable-sandbox');
+
+        server.close();
       });
 
       it('should open windows with the options configured via setWindowOpenHandler handlers', async () => {
@@ -3731,15 +3741,25 @@ describe('BrowserWindow module', () => {
           }
         });
 
+        const server = http.createServer((req, res) => {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end('<html><body>Blank Page</body></html>');
+        });
+        const serverUrl = (await listen(server)).url;
+
         const preloadPath = path.join(mainFixtures, 'api', 'new-window-preload.js');
         w.webContents.setWindowOpenHandler(() => ({ action: 'allow', overrideBrowserWindowOptions: { webPreferences: { preload: preloadPath, contextIsolation: false } } }));
-        w.loadFile(path.join(fixtures, 'api', 'new-window.html'));
+        await w.loadFile(path.join(fixtures, 'api', 'new-window.html'), {
+          query: { url: serverUrl }
+        });
         const [[, childWebContents]] = await Promise.all([
           once(app, 'web-contents-created') as Promise<[any, WebContents]>,
           once(ipcMain, 'answer')
         ]);
         const webPreferences = childWebContents.getLastWebPreferences();
         expect(webPreferences!.contextIsolation).to.equal(false);
+
+        server.close();
       });
 
       it('should set ipc event sender correctly', async () => {
@@ -3909,10 +3929,20 @@ describe('BrowserWindow module', () => {
         expect(content).to.equal('Hello');
       });
       it('blocks accessing cross-origin frames', async () => {
+        const server = http.createServer((req, res) => {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end('<html><body>Blank Page</body></html>');
+        });
+        const serverUrl = (await listen(server)).url;
+
         const answer = once(ipcMain, 'answer');
-        w.loadFile(path.join(fixtures, 'api', 'native-window-open-cross-origin.html'));
+        await w.loadFile(path.join(fixtures, 'api', 'native-window-open-cross-origin.html'), {
+          query: { url: serverUrl }
+        });
         const [, content] = await answer;
         expect(content).to.equal('Failed to read a named property \'toString\' from \'Location\': Blocked a frame with origin "file://" from accessing a cross-origin frame.');
+
+        server.close();
       });
       it('opens window from <iframe> tags', async () => {
         const answer = once(ipcMain, 'answer');
@@ -3973,6 +4003,12 @@ describe('BrowserWindow module', () => {
         await webviewLoaded;
       });
       it('should open windows with the options configured via setWindowOpenHandler handlers', async () => {
+        const server = http.createServer((req, res) => {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end('<html><body>Blank Page</body></html>');
+        });
+        const serverUrl = (await listen(server)).url;
+
         const preloadPath = path.join(mainFixtures, 'api', 'new-window-preload.js');
         w.webContents.setWindowOpenHandler(() => ({
           action: 'allow',
@@ -3983,13 +4019,17 @@ describe('BrowserWindow module', () => {
             }
           }
         }));
-        w.loadFile(path.join(fixtures, 'api', 'new-window.html'));
+        await w.loadFile(path.join(fixtures, 'api', 'new-window.html'), {
+          query: { url: serverUrl }
+        });
         const [[, childWebContents]] = await Promise.all([
           once(app, 'web-contents-created') as Promise<[any, WebContents]>,
           once(ipcMain, 'answer')
         ]);
         const webPreferences = childWebContents.getLastWebPreferences();
         expect(webPreferences!.contextIsolation).to.equal(false);
+
+        server.close();
       });
 
       describe('window.location', () => {
