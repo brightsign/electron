e4f3087cef4ef1aef8f73ae987e5c31be36fdfcc
Tariq Bashir <120014322+t-bashir-bs@users.noreply.github.com>
2026-05-06 08:38:16 +0100

    feat: Add hideScrollBars BrowserWindow constructor param (#19)
    * OS-15351: Add hideScrollBars BrowserWindow constructor param
    
    * tests: Add hideScrollBars test case to BrowserWindow spec
    
    * tests: Add vertical scrollbar check
    
    Cherry picked from:
    f59a20c6e4d620d34626659fc17718924ced44ea: feat: Add hideScrollBars BrowserWindow constructor param (#19)

--- shell/browser/web_contents_preferences.cc
+++ shell/browser/web_contents_preferences.cc
@@ -123,6 +123,7 @@ void WebContentsPreferences::Clear() {
   text_areas_are_resizable_ = true;
   webgl_ = true;
   enable_preferred_size_mode_ = false;
+  hide_scroll_bars_ = false;
   web_security_ = true;
   allow_running_insecure_content_ = false;
   offscreen_ = false;
@@ -183,6 +184,7 @@ void WebContentsPreferences::SetFromDictionary(
   web_preferences.Get(options::kWebGL, &webgl_);
   web_preferences.Get(options::kEnablePreferredSizeMode,
                       &enable_preferred_size_mode_);
+  web_preferences.Get(options::kHideScrollBars, &hide_scroll_bars_);
   web_preferences.Get(options::kWebSecurity, &web_security_);
   if (!web_preferences.Get(options::kAllowRunningInsecureContent,
                            &allow_running_insecure_content_) &&
@@ -471,6 +473,7 @@ void WebContentsPreferences::OverrideWebkitPrefs(
   prefs->v8_cache_options = v8_cache_options_;
 
   prefs->dom_paste_enabled = deprecated_paste_enabled_;
+  prefs->hide_scrollbars = hide_scroll_bars_;
 }
 
 WEB_CONTENTS_USER_DATA_KEY_IMPL(WebContentsPreferences);
